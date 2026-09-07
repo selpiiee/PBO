1b97e320759a708a16dde22711eb680cb42c14d2
selpiiee <selvynurkhoffifa@gmail.com>
2026-09-07 19:52:21 +0700

    Experiment 2 - Question 4 & 5

--- jobsheet3/Motor.java
+++ jobsheet3/Motor.java
@@ -28,7 +28,18 @@ public class Motor {
     public void setSpeed(int speed) {
         if (!this.isEngineOn && speed > 0) {
             System.out.println("The speed must not exceed 0 when the engine is OFF");
-        } else {
+        } 
+        //experiment 2 - question number 4
+        else if (speed > 100) {
+            System.out.println("Speed cannot exceed 100!");
+            this.speed = 100;
+        } 
+        //experiment 2 - question number 5
+        else if (speed < 0) {
+            System.out.println("Spedd cannot be negative!");
+            this.speed = 0;
+        } 
+        else {
             this.speed = speed;
         }
     }
